3cbdfa589c22818c55c2d9436cc11727b84e63a5
NituRay24 <raynitu24@gmail.com>
2022-06-03 01:14:55 +0530

    Day 15th Assignment

---
+++ src/HashTable.java
@@ -0,0 +1,34 @@
+import java.util.Hashtable;
+import java.util.Map;
+public class HashTable {
+    Hashtable<String, Integer> friendsTable = new Hashtable<>();
+
+    public void defineHashTable() {
+// insert values into the hashtable
+        friendsTable.put("Nitu", 25);
+        friendsTable.put("Archana",26 );
+        friendsTable.put("Kritika", 27);
+        System.out.println(friendsTable);
+    }
+
+    // update values into the hashtable
+    public void updateHashTable() {
+        defineHashTable();
+        friendsTable.put("Nitu", 25);
+        System.out.println(friendsTable);
+    }
+
+    // To remove a value
+    public void removeAnElement() {
+        updateHashTable();
+        friendsTable.remove("Nitu");
+        System.out.println(friendsTable);
+    }
+
+    public static void main(String[] args) {
+        HashTable htd = new HashTable();
+        htd.defineHashTable();
+        htd.updateHashTable();
+
+    }
+}
